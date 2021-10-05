8eeb238ef9e9496d1cb9bf649d3ce8d8b6a597e7
Shyvanth <kavalishyvanthkumarreddy@gmail.com>
2021-10-05 11:34:48 +0530

    1st half

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+import Auth from "./API/Auth";
+import ConnectDB from "./database/connection";
+
+const zomato=express();//initialsion
+zomato.use(express.json());
+zomato.use(express.urlencoded({extended:false}));
+zomato.use(cors());
+zomato.use(helmet());
+zomato.use("/auth",Auth);
+
+zomato.get("/", (req,res) => res.json({message: "SetUp Success Yay!!"}));
+
+zomato.listen(420,()=>
+ConnectDB().then(()=>console.log("modalpettandi"))
+.catch(()=>console.log("try again failed")));
