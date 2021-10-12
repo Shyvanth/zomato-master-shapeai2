3f5c9a06207e40c39fc24c18c751f3bea36bb679
Shyvanth <kavalishyvanthkumarreddy@gmail.com>
2021-10-12 21:45:00 +0530

    day 59

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-export default async()=>{
-    return mongoose.connect(process.env.MONGO-URL,{});
-   
-
+export default async () => {
+  return mongoose.connect(process.env.MONGO_URL,{
+    
+  });
 };
--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
+
+import googleConfig from "./config/google.config";
 
 import Auth from "./API/Auth";
+import Restaurant from "./API/Restaurant";
+
 import ConnectDB from "./database/connection";
 
 const zomato=express();//initialsion
@@ -12,6 +17,13 @@ zomato.use(express.urlencoded({extended:false}));
 zomato.use(cors());
 zomato.use(helmet());
 zomato.use("/auth",Auth);
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+googleAuthConfig(passport);
+
+zomato.use("/restaurant",Restaurant);
+
 
 zomato.get("/", (req,res) => res.json({message: "SetUp Success Yay!!"}));
 
